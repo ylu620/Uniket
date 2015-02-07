a957bffaeae97fed54e733e5548dcb3635057561
Jun Suh Lee <junsuhle@itssal109-25.ics.usc.edu>
2015-02-07 13:49:59 -0800

    Initial Commit

---
+++ Fleem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Fleem
+//
+//  Created by Jun Suh Lee on 2/7/15.
+//  Copyright (c) 2015 ___Fleem___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Fleem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fleem
+//
+//  Created by Jun Suh Lee on 2/7/15.
+//  Copyright (c) 2015 ___Fleem___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
